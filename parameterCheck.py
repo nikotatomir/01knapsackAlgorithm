f7c50f4b72e7181344913a7390a514e6e3ce29e5
nikotatomir <niko.tatomir@gmail.com>
2022-03-23 23:01:40 +0100

    functions in parameterCheck.py that execute all the checks to ensure proper problem setup are created and tested.

--- parameterCheck.py
+++ parameterCheck.py
@@ -5,26 +5,59 @@ import sys
 
 class parameterCheck:
 
-	def __init__(self, numberOfItems, maxWeightCapacity, weights = None, values = None):
+	def __init__(self, numberOfItems, maxWeightCapacity, randomWeightLimit = None, randomValueLimit = None, weights = None, values = None):
 
-		self.numberOfItems = numberOfItems
-		self.maxWeightCapacity = maxWeightCapacity
+		self.__numberOfItems = numberOfItems
+		self.__maxWeightCapacity = maxWeightCapacity
 		
-		self.executeGeneralCheck()
+		self.__randomWeightLimit = randomWeightLimit
+		self.__randomValueLimit = randomValueLimit
 
-		if weights is None and values is None:
+		self.__weights = weights
+		self.__values = values
+
+		self.generalCheck()
+
+		if self.__weights is None and self.__values is None and self.__randomWeightLimit is None and self.__randomValueLimit is None:
 			pass
+		elif self.__randomWeightLimit is not None and self.__randomValueLimit is not None and self.__weights is None and self.__values is None: 
+			# random problem setup
+			self.checkRandomArrayWeightValueLimit()
+		elif self.__weights is not None and self.__values is not None and self.__randomWeightLimit is None and self.__randomValueLimit is None: 
+			# manual problem setup
+			self.checkManualArrayLength()
+			self.checkManualArrayForPositiveIntegers()
 		else:
-			self.executeManualWeightsValuesCheck()
-			print(weights)
-			print(values)
-
+			pass
 
 	# function that checks if numberOfItems and maxWeightCapacity are positive integers
-	def executeGeneralCheck(self):
-		pass
+	def generalCheck(self):
+		if self.__numberOfItems <= 0 or not isinstance(self.__numberOfItems, int):
+			sys.exit("\n***EXITING PROGRAM***\nError Message: numberOfItems must be a positive nonzero integer")
+		elif self.__maxWeightCapacity <= 0 or not isinstance(self.__maxWeightCapacity, int):
+			sys.exit("\n***EXITING PROGRAM***\nError Message: maxWeightCapacity must be a positive nonzero integer")
+		else:
+			pass
 
 	# function that checks if the length of the manually created weights and values arrays are equal to numberOfItems variable 
-	# also checks if the weight and values arrays are positive integers
-	def executeManualWeightsValuesCheck(self):
-		pass
+	def checkManualArrayLength(self):
+		if len(self.__weights) != self.__numberOfItems or len(self.__values) != self.__numberOfItems:
+			sys.exit("\n***EXITING PROGRAM***\nError Message: The length of the weights and values arrays must be equal to the numberOfItems")
+		else:
+			pass
+	
+	# function that checks if weights and values arrays are all positive integers
+	def checkManualArrayForPositiveIntegers(self):
+		if np.any(self.__weights <= 0) or np.any(self.__values <= 0):
+			sys.exit("\n***EXITING PROGRAM***\nError Message: The weights and values arrays must be positive nonzero integers")
+		else:
+			pass
+
+	# function that checks if the upper limits for the randomly created weight and value arrays are positive nonzero integers greater than 1
+	def checkRandomArrayWeightValueLimit(self):
+		if self.__randomWeightLimit <= 1 or not isinstance(self.__randomWeightLimit, int):
+			sys.exit("\n***EXITING PROGRAM***\nError Message: randomItemWeightLimit must be a positive nonzero integer greater than 1")
+		elif self.__randomValueLimit <= 1 or not isinstance(self.__randomValueLimit, int):
+			sys.exit("\n***EXITING PROGRAM***\nError Message: randomItemValueLimit must be a positive nonzero integer greater than 1")
+		else:
+			pass 
--- run.py
+++ run.py
@@ -9,24 +9,24 @@ maxWeightCapacity = 7 # in kilograms, must be integer
 numberOfItems = 5 # number of items --> the size of the weights/values arrays defined below must be equal to the numberOfItems variable
 
 generationMethod = 'random' # 'manual' or 'random' --> specifices how the weights/values of the knapsack problem are generated
+
+# If generationMethod == 'random', define weights/values upper limits below
 randomItemWeightLimit = 7
-randomItemValueLimit = 100 
+randomItemValueLimit = 100
 
 # If generationMethod == 'manual', define weights/values below
 manualWeights = np.array([3,1,3,4,2], dtype = np.int32) # in kilograms
 manualValues = np.array([2,2,4,5,3], dtype = np.int32) # in dollars
-
+#-------------------------------------------#
 
 if generationMethod == 'manual':
 	parameterCheck(numberOfItems, maxWeightCapacity, weights = manualWeights, values = manualValues)
 	#solve = knapsackAlgorithm(numberOfItems, maxWeightCapacity, weights, values)
 elif generationMethod == 'random':
-	parameterCheck(numberOfItems, maxWeightCapacity)
+	parameterCheck(numberOfItems, maxWeightCapacity, randomWeightLimit = randomItemWeightLimit, randomValueLimit = randomItemValueLimit)
 	randomWeights = np.array([np.random.randint(1, randomItemWeightLimit) for i in range(numberOfItems)], dtype = np.int32)
 	randomValues = np.array([np.random.randint(1, randomItemValueLimit) for i in range(numberOfItems)], dtype = np.int32)
 	print(randomWeights, randomValues)
 	#solve = knapsackAlgorithm(numberOfItems, maxWeightCapacity, weights, values)
 else:
 	pass
-
-
